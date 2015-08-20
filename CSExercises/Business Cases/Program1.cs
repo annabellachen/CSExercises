1eff932899e75648c5400cdf7a1240dac7fdcfbc
annabellachen <554977238@qq.com>
2015-08-20 16:51:43 +0800

    day 5

---
+++ CSExercises/Business Cases/Program1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bus1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(TransferToWord(num));
+        }
+        static string TransferToWord(double num)
+        {
+            string result = null;
+            num = Math.Round(num, 2);
+            /*if (num >= 1000)
+            {
+                a1 = Convert.ToInt32(num / 1000);
+                num = num - 1000 * a1;
+            }
+            else a1 = 0;
+            if (num >= 100)
+            {
+                a2 = Convert.ToInt32(num / 100);
+                num = num - 100 * a2;
+            }
+            else a2 = 0;
+            if (num >= 10)
+            {
+                a3 = Convert.ToInt32(num / 10);
+                num = num - 10 * a3;
+            }
+            else a3 = 0;
+            if (num >= 1)
+            {
+                a4 = Convert.ToInt32(num / 1);
+                num = num - 1 * a4;
+            }
+            else a4 = 0;*/
+            double[,] cam = new double[6, 2] { { 1000, 0 }, { 100, 0 }, { 10, 0 }, { 1, 0 }, { 0.1, 0 }, { 0.01, 0 } };
+            string[,] res = new string[6, 2];
+            //res = null;
+            //caculate the number for 1000,100,10,1,0.1,0.01
+            for (int i = 0; i < 6; i++)
+            {
+                if (num >= cam[i, 0])
+                {
+                    cam[i, 1] = Convert.ToInt32(num / cam[i, 0]);
+                    num = num - cam[i, 0] * cam[i, 1];
+                    num = Math.Round(num, 2);
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                int temp = Convert.ToInt32(Math.Round(cam[i, 1]));
+                //give value to 1000 , 100, 1 and 0.01
+                if ((i != 2) && (i != 4))
+                {
+                    switch (temp)
+                    {
+                        case 0: res[i, 1] = ""; break;
+                        case 1: res[i, 1] = "One"; break;
+                        case 2: res[i, 1] = "Two"; break;
+                        case 3: res[i, 1] = "Three"; break;
+                        case 4: res[i, 1] = "Four"; break;
+                        case 5: res[i, 1] = "Five"; break;
+                        case 6: res[i, 1] = "Six"; break;
+                        case 7: res[i, 1] = "Seven"; break;
+                        case 8: res[i, 1] = "Eight"; break;
+                        case 9: res[i, 1] = "Nine"; break;
+                    }
+                }
+                //give value to 10 and 0.1
+                if ((i == 2) || (i == 4))
+                {
+                    switch (temp)
+                    {
+                        case 0: res[i, 1] = ""; break;
+                        case 1: res[i, 1] = "Ten"; break;
+                        case 2: res[i, 1] = "Twenty"; break;
+                        case 3: res[i, 1] = "Thirty"; break;
+                        case 4: res[i, 1] = "Fourty"; break;
+                        case 5: res[i, 1] = "Fifty"; break;
+                        case 6: res[i, 1] = "Sixty"; break;
+                        case 7: res[i, 1] = "Seventy"; break;
+                        case 8: res[i, 1] = "Eighty"; break;
+                        case 9: res[i, 1] = "Ninety"; break;
+                    }
+                }
+                //
+                /*while ((i != 2) && (i != 4))
+                {
+                    switch (temp)
+                    {
+                        case 1: res[i, 1] = "One"; break;
+                        case 2: res[i, 1] = "Two"; break;
+                        case 3: res[i, 1] = "Three"; break;
+                        case 4: res[i, 1] = "Four"; break;
+                        case 5: res[i, 1] = "Five"; break;
+                        case 6: res[i, 1] = "Six"; break;
+                        case 7: res[i, 1] = "Seven"; break;
+                        case 8: res[i, 1] = "Eight"; break;
+                        case 9: res[i, 1] = "Nine"; break;
+                    }
+                }*/
+                switch (i)
+                {
+                    case 0: res[i, 0] = "Thousand"; break;
+                    case 1: res[i, 0] = "Hundred"; break;//don't forget "and" after hundred or thousand
+                    case 2: res[i, 0] = ""; break;
+                    case 3: res[i, 0] = ""; break;
+                    case 4: res[i, 0] = "Cents"; break;//before 0.1
+                    case 5: res[i, 0] = ""; break;
+
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                    result = result + res[i, j];
+            }
+            return result;
+        }
+    }
+
+}
+
---
+++ CSExercises/Business Cases/Program2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bus2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The old code:");
+            string ocode = "1852";
+            string blk = "932B";
+            string post = "Block 932B Singapore 1852";
+            string post1 = "Blk 932B Singapore 1852";
+            Console.WriteLine(ocode);
+            Console.WriteLine("The blk num:");
+            Console.WriteLine(TransferToNewCode(ocode, blk));
+            Console.WriteLine(TransferToCode(post));
+            Console.WriteLine(TransferToBlock(post));
+            Console.Write("The new one:");
+            Console.WriteLine(TransferToNewCode(TransferToCode(post), TransferToBlock(post)));
+        }
+        static string TransferToNewCode(string code, string blk)
+        {
+            string sp = null;
+            if (blk.Length > 3)
+            {
+                switch (blk.Substring(3, 1))
+                {
+                    case "A": sp = "1"; break;
+                    case "B": sp = "2"; break;
+                    case "C": sp = "3"; break;
+                    case "D": sp = "4"; break;
+                    case "E": sp = "5"; break;
+                    case "F": sp = "6"; break;
+                    case "G": sp = "7"; break;
+                    case "H": sp = "8"; break;
+                    default: sp = "0"; break;
+                }
+            }
+            code = code.Substring(2, 2) + sp + blk;
+            return code;
+        }
+        static string TransferToBlock(string post)
+        {
+            string[] arr = new string[4];
+            arr = post.Split(' ');
+            String blk = arr[1];
+            return blk;
+        }
+        static string TransferToCode(string post)
+        {
+            string[] arr = new string[4];
+            arr = post.Split(' ');
+            string code = arr[3];
+            return code;
+        }
+    }
+}
